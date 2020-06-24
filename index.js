f78d05081e2e98761b926cb71340fa8d817a0ec9
Oliver Cook <olliewatford@gmail.com>
2020-06-24 13:55:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var beatles=[]
 
 var theBeatlesPlay = (musicians,instruments) => {
-  for(let i=0;i<musicians.length:i++){
+  for(let i=0;i<musicians.length;i++){
     beatles.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return beatles
