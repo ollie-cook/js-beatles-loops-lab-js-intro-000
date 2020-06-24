add8ed3e4365758d16d0753943ac7eb9622263b2
Oliver Cook <olliewatford@gmail.com>
2020-06-24 13:55:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// add solution here
+var beatles=[]
+
+var theBeatlesPlay = (musicians,instruments) => {
+  for(let i=0;i<musicians.length:i++){
+    beatles.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return beatles
+}
